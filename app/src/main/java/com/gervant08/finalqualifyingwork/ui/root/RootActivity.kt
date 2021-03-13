76cd0e3383487aaf80d8c00e804c92c15c324bfd
Pavel Yakovlev <pavel.yakovlev.199@gmail.com>
2021-03-13 22:08:57 +0300

    main screen pre

--- app/src/main/java/com/gervant08/finalqualifyingwork/ui/root/RootActivity.kt
+++ app/src/main/java/com/gervant08/finalqualifyingwork/ui/root/RootActivity.kt
@@ -2,10 +2,14 @@ package com.gervant08.finalqualifyingwork.ui.root
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.gervant08.finalqualifyingwork.R
 import com.gervant08.finalqualifyingwork.app.MyApp
 import com.gervant08.finalqualifyingwork.model.data.DataStoreManager
 import com.gervant08.finalqualifyingwork.ui.authentication.AuthFragment
+import com.gervant08.finalqualifyingwork.ui.main.fragments.BasketFragment
+import com.gervant08.finalqualifyingwork.ui.main.fragments.HomeFragment
+import com.gervant08.finalqualifyingwork.ui.main.fragments.ProfileFragment
 
 class RootActivity : AppCompatActivity() {
     private val dataStoreManager = DataStoreManager(MyApp.applicationContext)
@@ -15,5 +19,7 @@ class RootActivity : AppCompatActivity() {
         val dataStoreManager = DataStoreManager(MyApp.applicationContext)
         val transaction = supportFragmentManager.beginTransaction()
                 .replace(R.id.container, AuthFragment(dataStoreManager)).commit()
+
+
     }
 }
