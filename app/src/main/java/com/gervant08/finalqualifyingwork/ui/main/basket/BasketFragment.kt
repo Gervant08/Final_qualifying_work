964fb2699c5203533f52ec6a6918a2ffa7dddcb5
Nikita <n.frankov76@gmail.com>
2021-03-15 12:42:29 +0300

    Refactoring

---
+++ app/src/main/java/com/gervant08/finalqualifyingwork/ui/main/basket/BasketFragment.kt
@@ -0,0 +1,9 @@
+package com.gervant08.finalqualifyingwork.ui.main.basket
+
+import androidx.fragment.app.Fragment
+import com.gervant08.finalqualifyingwork.R
+
+
+class BasketFragment : Fragment(R.layout.fragment_basket) {
+
+}
